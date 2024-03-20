88d23ced0548ae49691ba7706ecc701b2ea446b7
Francisco Nunez <francisconunez582@gmail.com>
2024-03-19 22:50:04 -0400

    p.71 while loop

---
+++ Ch01/01-06.cpp
@@ -0,0 +1,20 @@
+// Example of a while statement
+#include <iostream>
+using namespace std;
+
+int main() {
+    int countDown;
+
+    cout << "How many greetings do you want? ";
+    cin >> countDown;
+
+    while (countDown > 0) {
+        cout << "Hello ";
+        countDown = countDown -1;
+    }
+    cout << endl;
+
+    cout << "That's all!\n";
+
+    return 0;
+}
