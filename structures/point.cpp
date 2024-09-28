87aae3524294b08ffbcebef95d206b551982cc6e
Francisco Nunez <francisconunez582@gmail.com>
2024-09-27 23:16:04 -0400

    Updated file

--- structures/point.cpp
+++ structures/point.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
 
+using std::cin;
 using std::cout;
 using std::endl;
-using std::string;
+using std::string; // or std::string
 
 struct Point {
     double x, y;
